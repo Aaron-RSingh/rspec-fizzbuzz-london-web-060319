563268224d3320f38ead0b8874c83e507c11da93
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-27 14:17:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  "(int)
+def fizzbuzz(int)
   if int % 3 == 0 
     return "Fizz"
   elseif int % 5 == 0
@@ -10,5 +9,5 @@ def fizzbuzz
   elseif int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
   else !int % 3 == 0 && !int % 5 == 0
-    return"
+    return
 end
