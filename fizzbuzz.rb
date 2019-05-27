a9dbef881debe1ad25a4307f8a310121c8fbafa7
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-27 14:25:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,11 @@
 def fizzbuzz(int)
  if int % 3 == 0 
     "Fizz"
-
+ end
 #    elseif int % 5 == 0
  #   "Buzz"
 #  elseif int % 3 == 0 && int % 5 == 0
 #    return "FizzBuzz"
 #  else !int % 3 == 0 && !int % 5 == 0
 #    return
-end
 end
